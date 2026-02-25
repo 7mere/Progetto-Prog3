e9d1ea4f62dafcfcdd02700cb0056fcbffb214fc
valerioghirardotto <valerioghira@gmail.com>
2026-02-25 11:14:36 +0100

    Iniziostruttura client + configurazione per avviare più istanze

--- Client/src/main/java/com/unito/client/LoginController.java
+++ Client/src/main/java/com/unito/client/LoginController.java
@@ -3,7 +3,7 @@ package com.unito.client;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class LoginController {
     @FXML
     private Label welcomeText;
 

51f7d53716a0ae61f84b367c672214a61dce0d5e
valerioghirardotto <valerioghira@gmail.com>
2026-02-25 11:52:02 +0100

    Iniziata logica LoginController

--- Client/src/main/java/com/unito/client/LoginController.java
+++ Client/src/main/java/com/unito/client/LoginController.java
@@ -2,13 +2,34 @@ package com.unito.client;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import java.util.regex.Pattern;
 
 public class LoginController {
     @FXML
-    private Label welcomeText;
-
+    private TextField mailField;
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Label result;
+    @FXML
+    protected void onLoginButtonClick() {
+        String insertedMail = mailField.getText();
+        if(isValidEmail(insertedMail)) {
+            result.setText("L'indirizzo mail inserito è valido");
+        }else {
+            result.setText("La mail non è valida prova con un'altra");
+            mailField.clear();
+        }
     }
+
+    public static boolean isValidEmail(String email) {
+        if (email == null) return false;
+
+        String normalized = email.trim(); // rimuove spazi davanti/dietro (copi-incolla ecc.)
+        if (normalized.isEmpty()) return false;
+
+        String emailRegex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+        return Pattern.matches(emailRegex, normalized);
+    }
+
 }
+

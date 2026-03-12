fe2c57dc6a451987f52fda961ec953ee1301ee54
Luca Gado <luca.gado@edu.unito.it>
2026-03-12 16:02:26 +0100

    Try Commit

--- Server/src/main/java/com/unito/server/ClientHandler.java
+++ Server/src/main/java/com/unito/server/ClientHandler.java
@@ -1,19 +1,16 @@
 package com.unito.server;
 
-
-import com.unito.server.models.ServerStorage;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
+import com.unito.server.models.ServerStorage;
+
 public class ClientHandler implements Runnable {
 
     private final Socket clientSocket;
     private final ServerStorage model;
-
-
 
     public ClientHandler(Socket clientSocket, ServerStorage model) {
         this.clientSocket = clientSocket;
@@ -22,12 +19,12 @@ public class ClientHandler implements Runnable {
 
     @Override
     public void run() {
-
         try (
-                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true)
+                BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())); // reader per leggere i messaggi dal client
+                PrintWriter writer = new PrintWriter(clientSocket.getOutputStream(), true) // writer per inviare i messaggi al client
+                                                                                        // true per abilitare l'auto-flush, così ogni volta che scriviamo qualcosa con writer.println() viene inviato immediatamente al client senza dover chiamare writer.flush() manualmente
         ) {
-            String request = in.readLine();
+            String request = reader.readLine();
 
             if (request != null) {
                 System.out.println("Richiesta ricevuta dal client: " + request);
@@ -35,14 +32,15 @@ public class ClientHandler implements Runnable {
                 if (request.startsWith("LOGIN_CLICK|")) {
                     String email = request.substring("LOGIN_CLICK|".length());
                     System.out.println("L'utente " + email + " ha cliccato su login");
-                    out.println("OK");
+                    writer.println("OK");
                 } else {
                     System.out.println("Comando non riconosciuto");
-                    out.println("ERR");
+                    writer.println("ERR");
                 }
             }
 
         } catch (Exception e) {
+            // aggiunta un logger per loggare l'errore invece di stampare su console e inviare al client un messaggio di errore prima di chiudere la connessione
             System.err.println("Errore nel ClientHandler: " + e.getMessage());
             e.printStackTrace();
         } finally {
